8b88d5a8890f8c426d38385dccc1ebaf74ad34d9
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2016-01-25 20:05:12 +0530

    cover both constructor and non-constructor in all cases

--- tests/index.js
+++ tests/index.js
@@ -4,58 +4,83 @@ var ValueError = require('../');
 
 describe('ValueError should', function() {
 
-  it('return a valid ValueError instance when created as constructor function', function() {
+  it('return a valid ValueError instance', function() {
     expect(new ValueError()).to.be.an.instanceof(ValueError);
-  });
-
-  it('return a valid ValueError instance even when NOT created as constructor function', function() {
     expect(ValueError()).to.be.an.instanceof(ValueError);
   });
 
   it('return a ValueError with default message being empty string', function() {
+
     expect(function() {
       throw ValueError();
     }).to.throw(ValueError, /^ValueError$/);
+
+    expect(function() {
+      throw new ValueError();
+    }).to.throw(ValueError, /^ValueError$/);
+
   });
 
   it('return a ValueError with the message passed', function() {
+
     expect(function() {
       throw ValueError('Value is not correct');
     }).to.throw(ValueError, /^ValueError: Value is not correct$/);
+
+    expect(function() {
+      throw new ValueError('Value is not correct');
+    }).to.throw(ValueError, /^ValueError: Value is not correct$/);
+
   });
 
   it('have name as a read-only and non-enumerable property', function() {
-    var valueError = ValueError('Google');
-    expect(Object.getOwnPropertyDescriptor(valueError, 'name')).to.deep.equal({
+
+    expect(Object.getOwnPropertyDescriptor(ValueError('Google'), 'name')).to.deep.equal({
       'value': 'ValueError',
       'configurable': true,
       'enumerable': false,
       'writable': false
     });
+
+    expect(Object.getOwnPropertyDescriptor(new ValueError('Google'), 'name')).to.deep.equal({
+      'value': 'ValueError',
+      'configurable': true,
+      'enumerable': false,
+      'writable': false
+    });
+
   });
 
   it('have message as a writable, but non-enumerable property', function() {
-    var valueError = ValueError('Google');
-    expect(Object.getOwnPropertyDescriptor(valueError, 'message')).to.deep.equal({
+
+    expect(Object.getOwnPropertyDescriptor(ValueError('Google'), 'message')).to.deep.equal({
       'value': 'Google',
       'configurable': true,
       'enumerable': false,
       'writable': true
     });
+
+    expect(Object.getOwnPropertyDescriptor(new ValueError('Google'), 'message')).to.deep.equal({
+      'value': 'Google',
+      'configurable': true,
+      'enumerable': false,
+      'writable': true
+    });
+
   });
 
-  it('return a ValueError instance without itself in the stack trace when NOT created as constructor', function() {
+  it('return a ValueError instance without the name `ValueError` in the stack trace', function() {
+
     ValueError('Google').stack.split(/\s*\n\s*/).slice(1).forEach(function(line) {
       // replace all the file name part along with the line and column numbers
       expect(line.replace(/\/.*:\d+:\d+/, '')).to.not.contain('ValueError');
     });
-  });
 
-  it('return a ValueError instance without itself in the stack trace when created as constructor', function() {
     new ValueError('Google').stack.split(/\s*\n\s*/).slice(1).forEach(function(line) {
       // replace all the file name part along with the line and column numbers
       expect(line.replace(/\/.*:\d+:\d+/, '')).to.not.contain('ValueError');
     });
+
   });
 
 });
